ecb65bb76a8db7438c628a55ab349e933f375491
arvy <mayor.arvy@gmail.com>
2024-02-20 00:04:23 +0800

    added raw response model

---
+++ RainFallApi/RainFallApi/ApiProvider/RawResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace RainFallApi.ApiProvider;
+
+public class RawResponseModel
+{
+   [JsonProperty("@Id")]
+    public string Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public string Measure { get; set; }
+    public double Value { get; set; }
+}
+
